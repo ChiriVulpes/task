426ede07ed1fb4485d2a0598b1de708ec7f2ed15
ChiriVulpes <github@chiri.works>
2025-06-23 18:25:31 +0000

    Deploy 333f8dd: Make watch awaitWriteFinish stabilityThreshold way shorter

--- TaskRunner.js
+++ TaskRunner.js
@@ -148,7 +148,7 @@ const taskApi = {
         }
     },
     watch(globs, task, delay = 0) {
-        chokidar_1.default.watch(globs, { ignoreInitial: true, awaitWriteFinish: true })
+        chokidar_1.default.watch(globs, { ignoreInitial: true, awaitWriteFinish: { stabilityThreshold: 100 } })
             // eslint-disable-next-line @typescript-eslint/no-misused-promises
             .on('all', async (event, path) => {
             await sleep(delay);
